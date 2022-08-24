2cea028eecabb3f9f9c3a50b8bc5e465b6c90f53
estherdellacagna <86942596+estherdellacagna@users.noreply.github.com>
2022-08-24 15:04:40 +0200

    created the search engine

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import Dictionary from './Dictionary';
 
 export default function App() {
   return (
     <div className="App">
-    <p>
-      Hello 
-    </p>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="app-footer">
+        <small>This project was coded by: Esther Della Cagna</small>
+      </footer>
     </div>
   );
 }
