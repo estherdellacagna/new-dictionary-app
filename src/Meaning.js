2f6ede466c75bbaeada7b8c7badd210254574063
estherdellacagna <86942596+estherdellacagna@users.noreply.github.com>
2022-08-31 10:31:01 +0200

    synonyms bug

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   console.log(props.meaning);
@@ -10,9 +11,15 @@ export default function Meaning(props) {
         return (
           <div key={index}>
             <p>
+              <strong>Definition:</strong>
               {definition.definition}
               <br />
-              <strong>{definition.example}</strong>
+              <strong>Example: </strong>
+              <em>{definition.example}</em>
+              <br />
+             <Synonyms synonyms={props.result.meaning.synonyms} />
+
+              
             </p>
           </div>
         );
