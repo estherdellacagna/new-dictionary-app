158e7c3b2848f0ec92a39cf4d7eb4c1788dcd377
estherdellacagna <86942596+estherdellacagna@users.noreply.github.com>
2022-09-07 11:27:53 +0200

    added photos at the bottom

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,19 +2,31 @@ import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
 import Result from "./Result";
+import Photos from "./Photos";
 
 export default function Dictionary(props) {
   const [keyword, setKeyword] = useState(props.defaultKeyword);
   const [result, setResult] = useState(null);
-  const [loaded,setloaded]= useState(false);
+  const [loaded, setloaded] = useState(false);
+    const [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+
+  function handleDictionaryResponse(response) {
     setResult(response.data[0]);
   }
-
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
+  }
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    const pexelsApiKey =
+      "563492ad6f91700001000001c297f1ae5b6744f4ba5be70b76a31888";
+    const pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+
+    const headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
@@ -25,26 +37,30 @@ export default function Dictionary(props) {
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
-  function load(){
+  function load() {
     setloaded(true);
     search();
   }
-  if(loaded){
-  return (
-    <div className="Dictionary">
-      <section>
-        <h1>Dictionary App</h1>
-        <h2>What word would you like to look up?</h2>
-        <form onSubmit={handleSubmit}>
-          <input type="search" onChange={handleKeywordChange} defaultValue={props.defaultKeyword}/>
-        </form>
-        <div className="hint">Suggested words: Yoga, forest, sunset..</div>
-      </section>
-      <Result result={result} />
-    </div>
-  );
-  }
-  else{
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <section>
+          <h1>Dictionary App</h1>
+          <h2>What word would you like to look up?</h2>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="search"
+              onChange={handleKeywordChange}
+              defaultValue={props.defaultKeyword}
+            />
+          </form>
+          <div className="hint">Suggested words: Yoga, forest, sunset..</div>
+        </section>
+        <Result result={result} />
+        <Photos photos={photos} />
+      </div>
+    );
+  } else {
     load();
     return "Loading";
   }
--- src/Phonetic.js
+++ src/Phonetic.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./Phonetic.css";
 
 export default function Phonetic(props){
-    console.log(props.phonetic);
 return (
   <div className="phonetic">
     <a href={props.phonetic.audio} target="_blank" rel="noopener noreferrer">
